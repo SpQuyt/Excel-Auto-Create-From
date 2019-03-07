c69d74376a3f7cfd930437bfc8e2f9f8fe5184eb
SpQuyt <spquyt@gmail.com>
2019-03-07 19:02:07 +0700

    complete. much better

--- form.excel/src/main/java/fe/form/excel/So1Cua.java
+++ form.excel/src/main/java/fe/form/excel/So1Cua.java
@@ -8,18 +8,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class So1Cua {
 	private int approve = 0;
 	private File file;
-	public static int currentNumber = 0;
+	private JLabel label;
+	public static int currentNumber = 7;
 	private int checkRepetition = 0;
 
 	public int getCheckRepe() {
@@ -41,18 +44,49 @@ public class So1Cua {
 	public void setCheckRepe(int num) {
 		this.checkRepetition = num;
 	}
+	
+	public void checkLatestCode() throws IOException {
+		Cell cell;
+		Row row;
+		CellStyle style;
+		CellStyle[] stylearray;
+		XSSFFont font;
 
-	public void chooseFile(JFrame frame) {
+		FileInputStream inputStream = new FileInputStream(this.file);
+		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+		XSSFSheet sheet = workbook.getSheet("Sổ theo dõi 1 cửa CQ (nhập SL) ");
+		font = workbook.createFont();
+		font.setFontName("Times New Roman");
+		font.setFontHeight(8);
+
+		while (true) {
+			try {
+				if (sheet.getRow(currentNumber).getCell(0).getCellTypeEnum() == CellType.BLANK
+						|| sheet.getRow(currentNumber).getCell(0).getCellTypeEnum() == CellType._NONE) {
+					this.label.setText(sheet.getRow(currentNumber-1).getCell(0).getStringCellValue());
+					break;
+				}
+			} catch (NullPointerException e) {
+				break;
+			}
+			currentNumber++;
+		}
+		currentNumber = 7;
+		inputStream.close();
+	}
+
+	public void chooseFile(JFrame frame, JLabel lblChaBit) {
+		this.label = lblChaBit;
 		JFileChooser chooser = new JFileChooser("./");
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel files only", "xlsx", "xls");
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Chỉ được chọn file excel...", "xlsx", "xls");
 		chooser.addChoosableFileFilter(filter);
 		chooser.setAcceptAllFileFilterUsed(false);
-		
+
 		int returnVal = chooser.showOpenDialog(null);
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
 			System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
 			this.file = chooser.getSelectedFile();
-			
+
 			FileInputStream inputStream = null;
 			try {
 				inputStream = new FileInputStream(this.file);
@@ -87,6 +121,12 @@ public class So1Cua {
 		} else {
 
 		}
+		
+		try {
+			checkLatestCode();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public int checkClosed() {
@@ -103,17 +143,32 @@ public class So1Cua {
 		Cell cell;
 		Row row;
 		CellStyle style;
+		CellStyle[] stylearray;
+		XSSFFont font;
 
 		FileInputStream inputStream = new FileInputStream(this.file);
 		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
 		XSSFSheet sheet = workbook.getSheet("Sổ theo dõi 1 cửa CQ (nhập SL) ");
+		font = workbook.createFont();
+		font.setFontName("Times New Roman");
+		font.setFontHeight(8);
 
 		while (true) {
 			try {
-				if (sheet.getRow(currentNumber).getCell(0).getRawValue() == null) {
-					break;
-				} else if (sheet.getRow(currentNumber).getCell(0).getStringCellValue().compareTo(form.getMaSo()) == 0) {
-					this.checkRepetition = 1;
+				if (sheet.getRow(currentNumber).getCell(0).getCellTypeEnum() == CellType.NUMERIC) {
+					sheet.getRow(currentNumber).getCell(0).setCellType(CellType.STRING);
+					if (sheet.getRow(currentNumber).getCell(0).getStringCellValue().compareTo(form.getMaSo()) == 0) {
+						this.checkRepetition = 1;
+						break;
+					}
+				} else if (sheet.getRow(currentNumber).getCell(0).getCellTypeEnum() == CellType.STRING) {
+					if (sheet.getRow(currentNumber).getCell(0).getStringCellValue().compareTo(form.getMaSo()) == 0) {
+						this.checkRepetition = 1;
+						break;
+					}
+				} else if (sheet.getRow(currentNumber).getCell(0).getCellTypeEnum() == CellType.BLANK
+						|| sheet.getRow(currentNumber).getCell(0).getCellTypeEnum() == CellType._NONE) {
+					this.label.setText(sheet.getRow(currentNumber).getCell(0).getStringCellValue());
 					break;
 				}
 			} catch (NullPointerException e) {
@@ -123,19 +178,57 @@ public class So1Cua {
 		}
 
 		if (this.checkRepetition == 0) {
+			stylearray = new CellStyle[13];
+			for (int i = 0; i < 13; i++) {
+				stylearray[i] = sheet.getRow(currentNumber - 1).getCell(i).getCellStyle();
+			}
+
 			row = sheet.createRow(currentNumber);
+
 			cell = row.createCell(0, CellType.STRING);
+			cell.setCellStyle(stylearray[0]);
 			cell.setCellValue(form.getMaSo());
+
 			cell = row.createCell(1, CellType.STRING);
+			cell.setCellStyle(stylearray[1]);
 			cell.setCellValue(form.getNguonKinhPhi());
+
 			cell = row.createCell(2, CellType.STRING);
+			cell.setCellStyle(stylearray[2]);
 			cell.setCellValue(form.getKhoaPhong());
+
 			cell = row.createCell(3, CellType.STRING);
+			cell.setCellStyle(stylearray[3]);
 			cell.setCellValue(form.getSoHoatDong());
+
 			cell = row.createCell(4, CellType.STRING);
+			cell.setCellStyle(stylearray[4]);
 			cell.setCellValue(form.getNoiDung());
-			cell = row.createCell(5, CellType.NUMERIC);
+
+			cell = row.createCell(5, CellType.STRING);
+			cell.setCellStyle(stylearray[5]);
 			cell.setCellValue(form.getSoTien());
+			
+			cell = row.createCell(6, CellType.STRING);
+			cell.setCellStyle(stylearray[6]);
+			
+			cell = row.createCell(7, CellType.STRING);
+			cell.setCellStyle(stylearray[7]);
+			
+			cell = row.createCell(8, CellType.STRING);
+			cell.setCellStyle(stylearray[8]);
+			
+			cell = row.createCell(9, CellType.STRING);
+			cell.setCellStyle(stylearray[9]);
+			
+			cell = row.createCell(10, CellType.STRING);
+			cell.setCellStyle(stylearray[10]);
+			
+			cell = row.createCell(11, CellType.STRING);
+			cell.setCellStyle(stylearray[11]);
+			
+			cell = row.createCell(12, CellType.STRING);
+			cell.setCellStyle(stylearray[12]);
 
 			inputStream.close();
 
@@ -143,6 +236,7 @@ public class So1Cua {
 			FileOutputStream out = new FileOutputStream(file);
 			try {
 				workbook.write(out);
+				this.label.setText(sheet.getRow(currentNumber).getCell(0).getStringCellValue());
 				so1cuathongbao.setText(form.getMaSo() + " đã được thêm vào sổ 1 cửa!");
 				out.close();
 			} catch (IOException e) {
@@ -152,6 +246,6 @@ public class So1Cua {
 		} else {
 			JOptionPane.showMessageDialog(null, "Mã số " + form.getMaSo() + " đã bị trùng!");
 		}
-		currentNumber = 0;
+		currentNumber = 7;
 	}
 }
